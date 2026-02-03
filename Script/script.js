134f79e3ad396e5abc7bf4fbf2e04d461726ac5c
Felipe <lipe.conta1632@gmail.com>
2026-02-02 22:48:51 -0300

    Adicionado banco de imagens, css iniciado, estrutura do HTML sendo desenvolvida, API instaurada e funcionando.

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,17 @@
+const key = '6bb6a1e9'
+const url = 'https://api.hgbrasil.com/weather?key=6bb6a1e9&format=json-cors';
+
+function carregarClima() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log("Sucesso! Veja os dados abaixo:");
+        console.log(data);
+
+        if (data.results) {
+            console.log(`Temperatura em ${data.results.city}: ${data.results.temp}°C |||| Velocidade do vento: ${data.results.wind_speedy} | Fase da lua${data.results.moon_phase}`)
+        }
+    })
+    .catch(error => console.error("Erro na requisição", error));
+}
 
