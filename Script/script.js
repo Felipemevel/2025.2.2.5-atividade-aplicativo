6e1fe801683a55844df6568cc034689ae3897fb4
Felipe <lipe.conta1632@gmail.com>
2026-02-03 11:15:58 -0300

    Atividade progredindo, adicionado o card principal, estilização básica do css, api respondendo ao input

--- Script/script.js
+++ Script/script.js
@@ -1,12 +1,24 @@
 const key = '6bb6a1e9'
-const url = 'https://api.hgbrasil.com/weather?key=6bb6a1e9&format=json-cors';
+const url = `https://api.hgbrasil.com/weather?key=${key}&format=json-cors`;
+const nomeCidade = document.getElementById('input-localizacao').value;
 
-function carregarClima() {
-    fetch(url)
+const inputLocalizacao = document.getElementById('input-localizacao');
+const displayNomeCidade = document.getElementById('nome-cidade');
+const temperaturaAtual = document.getElementById('temperatura-atual');
+const dataAtual = document.getElementById('data');
+
+function carregarClima(cidade = "") {
+
+    const urlFinal = cidade ? `${url}&city_name=${cidade}` : url;
+
+    fetch(urlFinal)
     .then(response => response.json())
     .then(data => {
-        console.log("Sucesso! Veja os dados abaixo:");
-        console.log(data);
+
+        const res = data.results;
+        displayNomeCidade.innerText = res.city;
+        temperaturaAtual.innerText = `${res.temp}°C`;
+        dataAtual.innerText = `Data: ${res.date} - ${res.time}`;
 
         if (data.results) {
             console.log(`Temperatura em ${data.results.city}: ${data.results.temp}°C |||| Velocidade do vento: ${data.results.wind_speedy} | Fase da lua${data.results.moon_phase}`)
@@ -15,3 +27,15 @@ function carregarClima() {
     .catch(error => console.error("Erro na requisição", error));
 }
 
+inputLocalizacao.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        const valorDigitado = inputLocalizacao.value;
+        if (valorDigitado !== "") {
+            displayNomeCidade.innerText = "Buscando...";
+            carregarClima(valorDigitado);
+        }
+        inputLocalizacao.value = "";
+    }
+});
+
+carregarClima();
